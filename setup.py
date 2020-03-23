69049ce9481d845b0de9bd820b2f57b89c339694
Ubuntu <ubuntu@ip-172-31-25-94.us-west-2.compute.internal>
2020-03-23 00:30:53 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = 'despair',
+    packages = ['despai'],
+    version = '0.1',
+    license='MIT',
+    author = 'selfquery',
+    author_email = 'pypi@selfquery.dev',
+    url = 'https://github.com/selfquery/despair',
+    keywords = ['python', 'database', 'pickledb', 'despair'],
+    install_requires=[       
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
